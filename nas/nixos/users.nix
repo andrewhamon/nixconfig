aea62002c392645a727a6d837207afdb5c6ae154
Andrew Hamon <and.ham95@gmail.com>
2022-12-29 18:55:24 -0800

    resuce usage of nix-env

--- nas/nixos/users.nix
+++ nas/nixos/users.nix
@@ -21,5 +21,8 @@
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMyVXFKM5PhtaIN+vmGjtHLj2X7sxbefSRU68SnLopPK pi@jwst"
     ];
+    packages = [
+      pkgs.imagemagick
+    ];
   };
 }
