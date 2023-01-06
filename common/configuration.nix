1d2f08ba0c0def0814af1ab8356df97e72656204
Andrew Hamon <and.ham95@gmail.com>
2023-01-05 21:50:59 -0800

    Add tmux to common

--- common/configuration.nix
+++ common/configuration.nix
@@ -33,4 +33,6 @@
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINNcsvEP/ZAEHTYgqahtTUoWpw18qoo3G4iObCGVwCGq"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIkO+UDBJuvpmTa6EriH1TJdJTx+YB/4uv4LmM+5mOp5"
   ];
+
+  programs.tmux.enable = true;
 }
