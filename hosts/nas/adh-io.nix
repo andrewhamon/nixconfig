1ed768a4b3a3ff152408874dc8f1a6b1d5ce92b5
Andrew Hamon <and.ham95@gmail.com>
2023-12-22 21:49:20 -0600

    add adh.io

---
+++ hosts/nas/adh-io.nix
@@ -0,0 +1,28 @@
+{...}:
+let
+  resume = builtins.fetchTarball {
+      url = "https://github.com/andrewhamon/hamon.cc/archive/f4171aef9fecd7f87625c1f1a6fc5e7bc36372aa.tar.gz";
+      sha256 = "sha256:1bnjclmk1v3yy83hd76rlsavg5smm1jzr2dnx75g5cdwdrm545ir";
+    };
+  resumePath = "${resume}";
+in
+{
+  services.nginx.virtualHosts."adh.io" = {
+    enableACME = true;
+    onlySSL = true;
+
+    root = resumePath;
+  };
+
+  services.nginx.virtualHosts.default = {
+    locations."/".return = "301 https://$host$request_uri";
+    default = true;
+    listen = [
+      {
+        addr = "0.0.0.0";
+        port = 80;
+        ssl = false;
+      }
+    ];
+  };
+}
