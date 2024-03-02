4b94b4bf8430263846355c68ffb2988d9a623d7d
Andy Hamon <andy.hamon@discordapp.com>
2024-03-01 17:26:27 -0800

    make kitty less stupid

--- home/andrewhamon/desktop.nix
+++ home/andrewhamon/desktop.nix
@@ -10,6 +10,8 @@
     extraConfig = ''
       map alt+left send_text all \x1b\x62
       map alt+right send_text all \x1b\x66
+      scrollback_lines 1000000
+      term xterm-256color
     '' + lib.optionalString pkgs.stdenv.isLinux ''
       map ctrl+v paste_from_clipboard
       map ctrl+c copy_or_interrupt
