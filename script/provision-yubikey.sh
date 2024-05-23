899b55d2ace135b3c283b140f34aded8cef19a09
Andy Hamon <andy.hamon@discordapp.com>
2024-05-23 14:53:07 -0700

    Set fido PIN on yubikey

--- script/provision-yubikey.sh
+++ script/provision-yubikey.sh
@@ -1,4 +1,5 @@
 name="$1"
 
+ykman fido access change-pin 
 ssh-keygen -t ed25519-sk -N "" -C "and.ham95@gmail.com" -O "application=ssh:$name" -O resident
 age-plugin-yubikey --generate --name "$name" --touch-policy cached --pin-policy once
